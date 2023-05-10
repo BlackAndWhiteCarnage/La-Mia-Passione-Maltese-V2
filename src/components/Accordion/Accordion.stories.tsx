408383e1b9be5fe9ad4dfb90916e72adbe7f432b
unknown <krzys.repsch@gmail.com>
2023-05-10 16:18:21 +0200

    feat: add Accordion component

---
+++ src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,30 @@
+/**
+ * External dependencies
+ */
+import { StoryObj } from '@storybook/react';
+
+/**
+ * Internal dependencies
+ */
+import Accordion from '.';
+
+export default {
+	title: 'Components/Accordion',
+	component: Accordion,
+};
+
+export const Default: StoryObj<typeof Accordion> = {
+	args: {
+		items: new Array(6).fill({
+			label: 'Jaki charakter będzie miał mój Maltańczyk?',
+			children: (
+				<p className="is-style-p-large">
+					Charakteru nie da się tak naprawdę określić, tak samo jak z
+					dziećmi. Wszystko zależy od wychowania. Pies w pełni
+					fizycznie i emocjonalnie rozwija się do drugiego roku życia.
+					Mamy obecnie cztery pieski a każdy to inny świat.
+				</p>
+			),
+		}),
+	},
+};
