ae92abf41ccd4a97e8bf1b2b8116307580720850
unknown <krzys.repsch@gmail.com>
2023-05-10 19:37:09 +0200

    feat: adjust Contact styles

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -7,9 +7,10 @@ import { FC } from 'react';
  * Internal dependencies
  */
 import { SectionWrapper, ContactForm } from '@/components';
+import classes from './Contact.module.scss';
 
 const Contact: FC = () => (
-	<SectionWrapper>
+	<SectionWrapper className={classes.wrapper}>
 		<ContactForm />
 	</SectionWrapper>
 );
