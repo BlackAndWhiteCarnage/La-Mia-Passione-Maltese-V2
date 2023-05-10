ab17a6088bf1be2236c2c4f94af58daf16d737ff
unknown <krzys.repsch@gmail.com>
2023-05-10 16:53:36 +0200

    feat: add ContactForm & FormField components

---
+++ src/components/ContactForm/ContactForm.stories.tsx
@@ -0,0 +1,27 @@
+/**
+ * External dependencies
+ */
+import { StoryObj, Meta, StoryFn } from '@storybook/react';
+
+/**
+ * Internal dependencies
+ */
+import ContactForm from '.';
+
+export default {
+	title: 'Components/ContactForm',
+	component: ContactForm,
+	decorators: [
+		(Story: StoryFn) => (
+			<div
+				style={{
+					maxWidth: '100rem',
+				}}
+			>
+				<Story />
+			</div>
+		),
+	],
+} satisfies Meta<typeof ContactForm>;
+
+export const Default: StoryObj<typeof ContactForm> = {};
