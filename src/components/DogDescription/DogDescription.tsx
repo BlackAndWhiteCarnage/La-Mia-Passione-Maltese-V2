64cf2cde3678c0119331e5d04fd6a3fea8d7e19d
unknown <krzys.repsch@gmail.com>
2023-05-10 20:36:01 +0200

    feat: add DogDescription animations

--- src/components/DogDescription/DogDescription.tsx
+++ src/components/DogDescription/DogDescription.tsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { FC } from 'react';
+import { motion } from 'framer-motion';
 import classnames from 'classnames/bind';
 
 /**
@@ -9,6 +10,8 @@ import classnames from 'classnames/bind';
  */
 import { ImageData } from '@/types';
 import { Title } from '@/components';
+import { scaleInAnimation, imageAnimation } from '@/config';
+import { useIntersectionObserver } from '@/hooks';
 import classes from './DogDescription.module.scss';
 
 type DogDescriptionProps = {
@@ -23,16 +26,31 @@ const DogDescription: FC<DogDescriptionProps> = ({
 	description,
 	image: { src, alt },
 	title,
-}) => (
-	<div className={classes.wrapper}>
-		<Title title={title} level={2} />
-		<div className={classes.inner}>
-			<p className={cx('description', 'is-style-p')}>{description}</p>
-			<div className={classes.imageWrap}>
-				<img src={src} alt={alt} className={classes.image} />
+}) => {
+	const { element, controls } = useIntersectionObserver();
+
+	return (
+		<motion.div
+			className={classes.wrapper}
+			ref={element}
+			variants={scaleInAnimation}
+			animate={controls}
+			initial="hidden"
+		>
+			<Title title={title} level={2} />
+			<div className={classes.inner}>
+				<p className={cx('description', 'is-style-p')}>{description}</p>
+				<div className={classes.imageWrap}>
+					<motion.img
+						src={src}
+						alt={alt}
+						className={classes.image}
+						variants={imageAnimation}
+					/>
+				</div>
 			</div>
-		</div>
-	</div>
-);
+		</motion.div>
+	);
+};
 
 export default DogDescription;
